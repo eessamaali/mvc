c8c9506718b00e7a63c4ffc1eec16870662f8678
Mohamed Essamaali <eessamaali996@gmail.com>
2023-12-17 21:17:18 -0500

    Added sln and mvc project

---
+++ WebGitHub/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using WebGitHub.Models;
+
+namespace WebGitHub.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Test()
+        {
+            var model = new MyModel();
+            var patern = @"~`(.+?)`~";
+            var rx = new Regex(patern);
+            string st = "I want to see ~`9`~ in my notes ~`19`~ ~`999`~ ~`0`~";
+             
+            MatchCollection matches = rx.Matches(st);
+            foreach (Match match in matches)
+            {
+                model.group0 += match.Groups[0].Value;
+                model.group1 += match.Groups[1].Value;
+                model.matches += 1;
+                var t = st.Contains(match.Groups[0].Value);
+                st = st.Replace(match.Groups[0].Value.ToString(), model.matches.ToString());
+            }
+            model.text = st;
+            return View(model);
+        }
+    }
+}
---
+++ WebGitHub/Models/MyModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebGitHub.Models
+{
+    public class MyModel
+    {
+        public string text { get; set; }
+        public int matches { get; set; }
+        public string group0 { get; set; }
+        public string group1 { get; set; }
+    }
+}
